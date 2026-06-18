da8f51a82ee4f16b5fbe987be7672607c4803f3b
Neeraj chauhan <nc1676639@gmail.com>
2026-06-18 18:25:34 +0530

    add data

---
+++ client/app/layout.js
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "My First Next App",
+  description: "Learning Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <nav>
+          <h1>My Website</h1>
+        </nav>
+
+        {children}
+
+        <footer>
+          <p>Copyright 2026</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ client/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Neeraj chauahn</h1>
+      <p>This is my first Next.js website</p>
+    </div>
+  );
+}
